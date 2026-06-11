0850e70998097d04bf61a70527399f068feed697
Brwonggg <Brandonwlc360@gmail.com>
2026-06-11 21:52:47 +0800

    Resolved input_features and output features mismatch

--- model.py
+++ model.py
@@ -1,26 +1,27 @@
 import torch
 from torch import nn
+from torchinfo import summary
 
-class Model(nn):
+class Model(nn.Module):
     def __init__(self, x):
-        super().__init__
+        super().__init__()
         self.conv_block = nn.Sequential(
-                            nn.Conv2d(in_channels=1 , out_channels=1 , kernel_size=3, stride=1, padding=1),
-                            nn.ReLU,
+                            nn.Conv2d(in_channels=1 , out_channels=32 , kernel_size=3, stride=1, padding=1),
+                            nn.ReLU(),
                             nn.Dropout(0.25),
-                            nn.Conv2d(in_channels=1 , out_channels=1 , kernel_size=3, stride=1, padding=1),
-                            nn.ReLU,
+                            nn.Conv2d(in_channels=32 , out_channels=64 , kernel_size=3, stride=1, padding=1),
+                            nn.ReLU(),
                             nn.Dropout(0.5),
                             nn.MaxPool2d(kernel_size=2)
         )
 
         self.classifier = nn.Sequential(
                             nn.Flatten(),
-                            nn.Linear(in_features=, out_features=)
-
+                            nn.Linear(in_features=64 * 24 * 24, out_features=2)
         )
 
     def forward(self, x):
         x = self.conv_block(x)
         x = self.classifier(x)
         return x
+    
