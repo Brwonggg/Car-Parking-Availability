9e29f8c839f3297bdf30aaef91fb7d8531ee6387
Brwonggg <Brandonwlc360@gmail.com>
2026-06-11 22:07:35 +0800

    Moved load_images()

--- image_loader.py
+++ image_loader.py
@@ -1,17 +1,6 @@
-import os
-from sklearn.model_selection import train_test_split
-import numpy as np
 import cv2 as cv
-from torch import nn
-import torch.nn.functional as F
-import torch
-from model import Model
-import tqdm
-import time
-
-train_data = ['/Users/brandon/Downloads/matchbox_cars_parkinglot/empty',
-              '/Users/brandon/Downloads/matchbox_cars_parkinglot/occupied'
-              ]
+import os
+import numpy as np
 
 def load_images(train_data):
     images = []
@@ -27,42 +16,3 @@ def load_images(train_data):
             except Exception as e:
                 print(f"Error loading image {os.path.join(folder,filename)}: {e}")
     return np.array(images), np.array(labels)
-
-images, labels = load_images(train_data)
-X_train, y_train, X_test, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
-
-X_train = X_train.reshape(X_train.shape[0],(48, 48), 1).astype('float32') / 255
-X_test = X_test.reshape(X_test.shape[0],(48, 48), 1).astype('float32') / 255
-
-y_train_long = torch.from_numpy(y_train).long()
-y_test_long = torch.from_numpy(y_test).long()
-y_train = F.one_hot(y_train_long)
-y_test = F.one_hot(y_test_long)
-
-print(X_train.dtype)
-
-epochs = 30 
-model = Model()
-optimizer = torch.optim.Adam(lr=0.01)
-loss_fn = nn.BCELoss()
-
-#training loop 
-for epoch in range(epochs):
-    model.train
-    
-    y_pred = model(X_train)
-
-    loss = loss_fn(y_pred, y_train)
-
-    optimizer.zero_grad()
-
-    loss.backward()
-
-    optimizer.step()
-
-#testing loop 
-model.eval()
-with torch.inference_mode:
-    test_pred = model(X_test)
-
-    test_loss = loss_fn(y_pred, y_test)
