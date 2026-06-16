a5d320cc82c170c41adab5847631bafb58aeaeea
Brwonggg <Brandonwlc360@gmail.com>
2026-06-16 15:25:06 +0800

    Added data augmentation

--- image_loader.py
+++ image_loader.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import os
 import numpy as np
 
-def load_images(train_data):
+def load_images(train_data, augment=True):
     images = []
     labels = []
     for i, folder in enumerate(train_data):
@@ -13,6 +13,23 @@ def load_images(train_data):
                 img = cv.resize(img, (48,48))
                 images.append(img)
                 labels.append(label)
+
+                if augment:
+                    # horizontal flip
+                    images.append(cv.flip(img, 1))
+                    labels.append(label)
+
+                    # slight rotation
+                    M = cv.getRotationMatrix2D((24, 24), 10, 1.0)
+                    rotated = cv.warpAffine(img, M, (48, 48))
+                    images.append(rotated)
+                    labels.append(label)
+
+                    # brightness adjustment
+                    brighter = cv.convertScaleAbs(img, alpha=1.2, beta=10)
+                    images.append(brighter)
+                    labels.append(label)
+
             except Exception as e:
                 print(f"Error loading image {os.path.join(folder,filename)}: {e}")
     return np.array(images), np.array(labels)
