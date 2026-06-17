cb72af710f9064931a92cbe16e74d3b41513cfdd
Brwonggg <Brandonwlc360@gmail.com>
2026-06-17 14:08:55 +0800

    Remove old files, update image_loader

--- image_loader.py
+++ image_loader.py
@@ -9,26 +9,10 @@ def load_images(train_data, augment=True):
         label = i
         for filename in os.listdir(folder):
             try:
-                img = cv.imread(os.path.join(folder,filename), cv.IMREAD_GRAYSCALE)
+                img = cv.imread(os.path.join(folder,filename))
                 img = cv.resize(img, (48,48))
                 images.append(img)
                 labels.append(label)
-
-                if augment:
-                    #Horizontal flip
-                    images.append(cv.flip(img, 1))
-                    labels.append(label)
-
-                    #Rotation
-                    M = cv.getRotationMatrix2D((24, 24), 10, 1.0)
-                    rotated = cv.warpAffine(img, M, (48, 48))
-                    images.append(rotated)
-                    labels.append(label)
-
-                    #Brightness
-                    brighter = cv.convertScaleAbs(img, alpha=1.2, beta=10)
-                    images.append(brighter)
-                    labels.append(label)
 
             except Exception as e:
                 print(f"Error loading image {os.path.join(folder,filename)}: {e}")
